887ede7dafc1c47709bcfa8f818c0f0023a780f4
Homxy <120322865+Homxy@users.noreply.github.com>
2023-01-03 16:35:53 +0700

    Create lab11_2.cpp

---
+++ lab11_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt");
+	
+	string textline;
+	dest<<"-------------------- BOOM ---------------------"<<endl;
+    while(getline(source, textline)){
+        dest<< textline <<endl;
+    }
+    dest<<"-------------------- HA!! ---------------------";
+    source.close();
+    dest.close();
+	return 0;
+}
