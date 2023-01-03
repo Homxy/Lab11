13c3eba71fda55c61dd08cba349fe0a97229b53c
Homxy <120322865+Homxy@users.noreply.github.com>
2023-01-03 16:36:39 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -31,14 +31,17 @@ int main(){
 	        if(grade=='0'){
 	            break;
 	        }   
-	        cout << "Wrong input. Please input again.";
+	        cout << "Wrong input. Please input again."<<endl;
 	    }
 	}while(true);
 	
 	
-	cout << "In total ? students.";
+	cout << "In total "<<i-1<<" students."<<endl;
 	cout << "A = " << count[0] <<", ";
 	cout << "B = " << count[1] <<", ";	
+	cout << "C = " << count[2] <<", ";
+	cout << "D = " << count[3] <<", ";
+	cout << "F = " << count[4];
 	//	and so on ... for grade = C, D, F	
 	
 	return 0;
